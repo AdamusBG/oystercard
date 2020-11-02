226871ba350a21d88893662a4d4f179a3f089994
Adam G <adamwizzy@hotmail.co.uk>
2020-11-02 15:09:53 +0000

    Added the feature for the card to be topped up & tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,4 +10,12 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
 
+  it "Responds to .top_up method" do
+    expect(subject).to respond_to(:top_up).with(1).argument
+  end
+
+  it "Balance increases by 5 when topped up by 5" do
+    expect { subject.top_up(5) }.to change { subject.balance }.by(5)
+  end
+
 end
