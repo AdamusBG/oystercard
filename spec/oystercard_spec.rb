31e47f2b267fccdddef776b4f394a9cbf8b35805
Adam G <adamwizzy@hotmail.co.uk>
2020-11-02 15:00:34 +0000

    Added oystercard w initial balance of 0 and tests

---
+++ spec/oystercard_spec.rb
@@ -0,0 +1,13 @@
+require 'oystercard'
+
+describe Oystercard do
+
+  it "Is an instance of the oystercard class" do
+    expect(subject).to be_instance_of(Oystercard)
+  end
+
+  it "Has a balance of 0 when set up by default" do
+    expect(subject.balance).to eq(0)
+  end
+
+end
